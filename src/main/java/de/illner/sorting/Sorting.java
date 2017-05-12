e7defcd39ca718ba97d2dd94f86ff83550d2e1d2
Robert Illner <r.illner@seeburger.de>
2017-05-12 10:55:56 +0200

    finished insertion sort for java.util.List

--- src/main/java/de/illner/sorting/Sorting.java
+++ src/main/java/de/illner/sorting/Sorting.java
@@ -1,6 +1,6 @@
 package de.illner.sorting;
 
-import java.util.Collections;
+
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
@@ -11,29 +11,32 @@ public final class Sorting
 {
 
     /**
-     * Although it is one of the elementary sorting algorithms with <code>O(n<sup>2</sup>)</code> worst-case time, insertion sort is the algorithm of choice either when the data is nearly sorted (because it is adaptive) or when the problem size is small (because it has low overhead). For these reasons, and because it is also stable, insertion sort is often used as the recursive base case (when the problem size is small) for higher overhead divide-and-conquer sorting algorithms, such as merge sort or quick sort.
-     * @param input
-     * @param comparator
-     * @return
+     * Although it is one of the elementary sorting algorithms with <code>O(n<sup>2</sup>)</code> worst-case time, insertion sort is the
+     * algorithm of choice either when the data is nearly sorted (because it is adaptive) or when the problem size is small (because it has
+     * low overhead). For these reasons, and because it is also stable, insertion sort is often used as the recursive base case (when the
+     * problem size is small) for higher overhead divide-and-conquer sorting algorithms, such as merge sort or quick sort.
+     *
+     * @param input the input {@link List} to be sorted
+     * @param comparator the {@link Comparator} for comparing the elements of the <code>input</code> List
+     * @return the sorted result {@link List} by using insertion sort
      */
-    public static <T, L extends List<T>> List<T> insertionSort(L input, Comparator<? super L> comparator)
+    public static <T> List<T> insertionSort(List<T> input, Comparator< ? super T> comparator)
     {
         Objects.requireNonNull(input, "input must not be null.");
+        Objects.requireNonNull(comparator, "comparator must not be null.");
+
         List<T> result = input.stream().collect(Collectors.toList());
-        if (input.size() < 2)
+
+        for (int i = 1; i < result.size(); i++)
         {
-            return result;
+            for (int k = i; k > 0 && comparator.compare(result.get(k), result.get(k - 1)) < 0; k--)
+            {
+                T tmp = result.get(k);
+                result.set(k, result.get(k - 1));
+                result.set(k - 1, tmp);
+            }
         }
-        
-        for (int k = 2; k <= input.size(); k++)
-        {
-            for (k = i; k > 1 and a[k] < a[k-1]; k--)
-        }
-        
+
         return result;
-        
     }
 }
-
-
-
--- src/test/java/de/illner/sorting/SortingTest.java
+++ src/test/java/de/illner/sorting/SortingTest.java
@@ -5,21 +5,124 @@
  */
 package de.illner.sorting;
 
-import java.util.ArrayList;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+import org.hamcrest.Matchers;
+import org.hamcrest.collection.IsIterableContainingInOrder;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 
 public class SortingTest
 {
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
 
     @Test
-    public void test()
+    public void insertionSortReverseSuccess()
     {
-        List<String> input = new ArrayList<>();
+        Comparator< ? super Integer> usedComparator = (s1, s2) -> s1.compareTo(s2);
+        List<Integer> input = Arrays.asList(new Integer(3), new Integer(2), new Integer(1), new Integer(0), new Integer(-1), new Integer(-2), new Integer(-3));
+        Integer[] expected = new Integer[]{new Integer(-3), new Integer(-2), new Integer(-1), new Integer(0), new Integer(1), new Integer(2), new Integer(3)};
+
+        List<Integer> output = Sorting.insertionSort(input, usedComparator);
+
+        assertThat(output, IsIterableContainingInOrder.contains(expected));
+    }
+
+
+    @Test
+    public void insertionSortFewUniqueReverseSuccess()
+    {
+        Comparator< ? super Integer> usedComparator = (s1, s2) -> s1.compareTo(s2);
+        List<Integer> input = Arrays.asList(new Integer(3), new Integer(1), new Integer(1), new Integer(0), new Integer(-2), new Integer(-2), new Integer(-3));
+        Integer[] expected = new Integer[]{new Integer(-3), new Integer(-2), new Integer(-2), new Integer(0), new Integer(1), new Integer(1), new Integer(3)};
+
+        List<Integer> output = Sorting.insertionSort(input, usedComparator);
+
+        assertThat(output, IsIterableContainingInOrder.contains(expected));
+    }
+
+
+    @Test
+    public void insertionSortSortedSuccess()
+    {
+        Comparator< ? super Integer> usedComparator = (s1, s2) -> s1.compareTo(s2);
+        List<Integer> input = Arrays.asList(new Integer(-3), new Integer(-2), new Integer(-1), new Integer(0), new Integer(1), new Integer(2), new Integer(3));
+        Integer[] expected = new Integer[]{new Integer(-3), new Integer(-2), new Integer(-1), new Integer(0), new Integer(1), new Integer(2), new Integer(3)};
+
+        List<Integer> output = Sorting.insertionSort(input, usedComparator);
+
+        assertThat(output, IsIterableContainingInOrder.contains(expected));
+    }
+
+
+    @Test
+    public void insertionSortSortedFewUniqueSuccess()
+    {
+        Comparator< ? super Integer> usedComparator = (s1, s2) -> s1.compareTo(s2);
+        List<Integer> input = Arrays.asList(new Integer(-3), new Integer(-2), new Integer(-2), new Integer(0), new Integer(1), new Integer(1), new Integer(3));
+        Integer[] expected = new Integer[]{new Integer(-3), new Integer(-2), new Integer(-2), new Integer(0), new Integer(1), new Integer(1), new Integer(3)};
+
+        List<Integer> output = Sorting.insertionSort(input, usedComparator);
+
+        assertThat(output, IsIterableContainingInOrder.contains(expected));
+    }
+
+
+    @Test
+    public void insertionSortInputNull()
+    {
+        Comparator< ? super Integer> usedComparator = (s1, s2) -> s1.compareTo(s2);
+
+        exception.expect(NullPointerException.class);
+        exception.expectMessage(Matchers.equalTo("input must not be null."));
+
+        Sorting.insertionSort(null, usedComparator);
+    }
+
+
+    @Test
+    public void insertionSortComparatorNull()
+    {
+        List<Integer> input = Arrays.asList(new Integer(3), new Integer(2), new Integer(1), new Integer(0), new Integer(-1), new Integer(-2), new Integer(-3));
+
+        exception.expect(NullPointerException.class);
+        exception.expectMessage(Matchers.equalTo("comparator must not be null."));
+
         Sorting.insertionSort(input, null);
     }
+
+
+    @Test
+    public void insertionSortInputEmpty()
+    {
+        Comparator< ? super Integer> usedComparator = (s1, s2) -> s1.compareTo(s2);
+        List<Integer> input = Collections.emptyList();
+
+        List<Integer> output = Sorting.insertionSort(input, usedComparator);
+
+        assertTrue(output.isEmpty());
+    }
+
+
+    @Test
+    public void insertionSortOneInputElement()
+    {
+        Comparator< ? super Integer> usedComparator = (s1, s2) -> s1.compareTo(s2);
+        List<Integer> input = Arrays.asList(new Integer(3));
+        Integer[] expected = new Integer[]{new Integer(3)};
+
+        List<Integer> output = Sorting.insertionSort(input, usedComparator);
+
+        assertThat(output, IsIterableContainingInOrder.contains(expected));
+    }
 }
-
-
-
